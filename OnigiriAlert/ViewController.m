89f7a68223f993f7ef33eefcf71e7b1412b2fc00
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-02-12 23:36:46 +0900

    Updated main view and app icon.

--- OnigiriAlert/ViewController.m
+++ OnigiriAlert/ViewController.m
@@ -10,9 +10,14 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
+@property (weak, nonatomic) IBOutlet UILabel *liveStatusLabel;
+
 @end
 
 @implementation ViewController
+
+#pragma mark - Object Lifecycle
 
 -(void)viewDidLoad
 {
@@ -20,10 +25,36 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self updateLiveStatus];
+}
+
 -(void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// #pragma mark - Property Methods
+// #pragma mark - [ClassName] Overrides
+// #pragma mark - [ProtocolName] Methods
+// #pragma mark - Public Interface
+
+#pragma mark - Internal Methods
+
+-(void)updateLiveStatus
+{
+    [self.activityIndicatorView startAnimating];
+    
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self.activityIndicatorView stopAnimating];
+        self.liveStatusLabel.text = @"live status here";
+    });
 }
 
 @end
